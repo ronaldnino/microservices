4a7c7ff64ce87906fcb084568c23a299b5ca694f
Ronald Niño <rnino@matrixcpmsolutions.com>
2018-02-17 13:42:49 -0400

    Estructura inicial

---
+++ frontends/admin/src/components/Sidebar/_nav.js
@@ -0,0 +1,15 @@
+export default {
+  items: [
+    {
+      name: 'Sistema practica',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+
+    },
+    {
+      name: 'Colors',
+      url: '/theme/colors',
+      icon: 'icon-drop',
+    }
+  ]
+};
